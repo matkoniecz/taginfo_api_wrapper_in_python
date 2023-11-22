079254bd6856ee04b944c4ff192b974a8be0cc4f
Mateusz Konieczny <matkoniecz@gmail.com>
2023-11-22 11:15:52 +0100

    python3.10 instead of python3.9 has now special support in reinstall script

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,8 @@ python3 setup.py sdist bdist_wheel
 cd dist
 pip3 uninstall taginfo -y
 pip3 install --user *.whl
-python3.9 -m pip uninstall taginfo -y
-python3.9 -m pip install --user *.whl
+python3.10 -m pip uninstall taginfo -y
+python3.10 -m pip install --user *.whl
 cd ..
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
