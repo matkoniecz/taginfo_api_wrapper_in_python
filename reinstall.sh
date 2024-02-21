411c6697feeda7b46646b625e33463c16634dca4
Mateusz Konieczny <matkoniecz@gmail.com>
2024-02-21 11:51:57 +0100

    generalize reinstall
    yes, it relies on code specific to my computer - let me know if you actually use this code

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,7 @@ python3 setup.py sdist bdist_wheel
 cd dist
 pip3 uninstall taginfo -y
 pip3 install --user *.whl
-python3.10 -m pip uninstall taginfo -y
-python3.10 -m pip install --user *.whl
 cd ..
+python3 ../../python_package_reinstaller/reinstaller.py taginfo # yes, it relies on code on my computer - let me know if anyone else wants to run this script
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
