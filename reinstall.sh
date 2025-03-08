e782b23b5fc09d832f8bf92187be160964e9f6ed
Mateusz Konieczny <matkoniecz@gmail.com>
2025-03-08 15:00:04 +0100

    update processing script

--- reinstall.sh
+++ reinstall.sh
@@ -1,9 +1,7 @@
 rm dist -rf
-python3 setup.py sdist bdist_wheel
-cd dist
-pip3 uninstall taginfo -y
-pip3 install --user *.whl
-cd ..
-python3 ../../python_package_reinstaller/reinstaller.py taginfo # yes, it relies on code on my computer - let me know if anyone else wants to run this script
-python3 -m unittest
+/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m build
+
+python3 ../../python_package_reinstaller/reinstaller.py dubious_tags # yes, it relies on code on my computer - let me know if anyone else wants to run this script
+
+/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m unittest
 # twine upload dist/* # to upload to PyPi
